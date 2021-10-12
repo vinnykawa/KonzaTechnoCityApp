9d2ee666ba99778879965d722e19bb41605983cc
Dennis Kawawa <denniskawawa@microsoft.com>
2021-10-12 11:22:54 +0300

    news component

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,55 +1,53 @@
-import React, {useEffect,useState} from "react";
-import { Text, Flatlist, StyleSheet, View, SafeAreaView } from "react-native";
-import {NewsItemView} from './../components/newsItems';
-import{Card} from '../components/mycomponents'
+import React, { useEffect, useState } from "react";
+import { Text, FlatList, StyleSheet, View, SafeAreaView } from "react-native";
+import { NewsItemView } from "./../components/newsItems";
+import { Card } from "../components/mycomponents";
 import { ScrollView } from "react-native-gesture-handler";
 
-function NewsScreen (){
-
+function NewsScreen() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   const getNews = async () => {
     try {
-     const response = await fetch('https://konza2.softwareske.net/wp-json/wp/v2/news/');
-     const json = await response.json();
+      const response = await fetch(
+        "https://live.konza.go.ke/wp-json/wp/v2/news/"
+      );
+      const json = await response.json();
 
-     console.log(json);
-     
-     setData(json);
-   } catch (error) {
-     console.error(error);
-   } finally {
-     setLoading(false);
-   }
- }
+      console.log(json);
 
- useEffect(() => {
-   getNews();
- }, []);
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
- console.log(data.title);
+  useEffect(() => {
+    getNews();
+  }, []);
 
- const renderItem = ({ item }) => (
-   <Text>{item.title}</Text>
-);
- 
-    return(
-      <SafeAreaView style={styles.container}>
-      <Flatlist
+  console.log(data.title);
+
+  const renderItem = ({ item }) => <Text>{item.title}</Text>;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
         data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.date}
+        renderItem={NewsItemView}
+        keyExtractor={(item) => item.date}
       />
     </SafeAreaView>
-    );
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    paddingTop: 22,
   },
   item: {
     padding: 10,
